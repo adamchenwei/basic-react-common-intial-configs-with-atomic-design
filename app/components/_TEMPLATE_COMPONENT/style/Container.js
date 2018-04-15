cad242b2eb7b6f8594cb786d2cdc99598348b6eb
Adam Chen Wei <adamchenwei@gmail.com>
2018-04-15 09:12:34 -0400

    chore(): remove dummy ref

--- app/components/_TEMPLATE_COMPONENT/style/Container.js
+++ app/components/_TEMPLATE_COMPONENT/style/Container.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
-import ComponentContainer from './../../../elements/ComponentContainer';
 
-export default styled(ComponentContainer)`
+export default styled.section`
   color: black;
 `;
